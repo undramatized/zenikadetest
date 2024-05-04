f3ebd8aeda5efdfca9018e4252b84cb341f053d2
Rama Krishna <siddarth21@hotmail.com>
2024-05-04 14:32:06 +0800

    add comments and error handling

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+import os
+
 import pandas as pd
 
 class ExcelReader:
+    """
+    A class to read and consolidate Excel data from multiple sheets.
+    """
     def __init__(self, src_path):
         self.df = None
         self.data = None
+        self.validate_path(src_path)
         self.source = src_path
         self.read_excel_sheets()
         self.consolidate_data_into_df()
 
+    def validate_path(self, path):
+        """
+        Check if the source Excel file exists.
+        """
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"The file {path} does not exist.")
+
     def read_excel_sheets(self):
-        self.data = pd.read_excel(self.source, sheet_name=None)
+        try:
+            self.data = pd.read_excel(self.source, sheet_name=None)
+        except Exception as e:
+            print(f"An error occurred while reading the file: {e}")
 
     def consolidate_data_into_df(self):
         all_months_data = []
@@ -22,7 +38,10 @@ class ExcelReader:
         self.df = pd.concat(all_months_data, ignore_index=True)
 
     def print_df(self):
-        print(self.df)
+        if self.df is not None:
+            print(self.df)
+        else:
+            print("No data to display.")
 
     def get_df(self):
         return self.df
